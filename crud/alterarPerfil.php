4da9917ae3679c42fd5c4c2ab8f52ed92b229d27
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-07-22 09:33:52 -0300

    configurando alterarPerfil.php

--- crud/alterarPerfil.php
+++ crud/alterarPerfil.php
@@ -13,22 +13,41 @@ $mysql = conectar();
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
-//verificar se o email existe no banco de dados.
-$consulta_usuario = executarSQL($mysql, "SELECT COUNT(*) FROM usuario WHERE email = '$email'");
-$quantidade_usuario = mysqli_fetch_row($consulta_usuario)[0];
+//verificar se o usuário não aletrou o seu email.
+$sql = "SELECT user.email FROM usuario user WHERE id_usuario = " . $_SESSION['usuario'][1];
 
-if ($quantidade_usuario != 0) {
-    
-    echo "O email já existe no sistema. <p><a href=\"../inicial.php\">Voltar</a></p>";
+$query = executarSQL($mysql, $sql);
 
-    die ();
-}else {
-    
-    $sql = "UPDATE usuario SET nome = '$nome', email = '$email' WHERE id_usuario = " . $_SESSION['usuario'][1];
+$userEmail = mysqli_fetch_array($query);
 
-    executarSQL($mysql, $sql);
+if ($userEmail['email'] == $email) {
+
+    $sql1 = "UPDATE usuario SET nome = '$nome' WHERE id_usuario = " . $_SESSION['usuario'][1];
+
+    executarSQL($mysql, $sql1);
 
     $_SESSION['usuario'][0] = $nome;
 
     header("location:../inicial.php");
+} else {
+
+    //verificar se o email existe no banco de dados.
+    $consulta_usuario = executarSQL($mysql, "SELECT COUNT(*) FROM usuario WHERE email = '$email'");
+    $quantidade_usuario = mysqli_fetch_row($consulta_usuario)[0];
+
+    if ($quantidade_usuario != 0) {
+
+        echo "O email já existe no sistema!";
+
+        die();
+    } else {
+
+        $sql2 = "UPDATE usuario SET nome = '$nome', email = '$email' WHERE id_usuario = " . $_SESSION['usuario'][1];
+
+        executarSQL($mysql, $sql2);
+
+        $_SESSION['usuario'][0] = $nome;
+
+        header("location:../inicial.php");
+    }
 }
